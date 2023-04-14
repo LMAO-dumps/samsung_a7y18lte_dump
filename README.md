26215603cf735df9c0e5d577008b1ac79c52709b
khushtaur3123 <khushtaur3123@gmail.com>
2023-04-14 16:10:52 +0530

    Add extras for a7y18ltejt-user 10 QP1A.190711.020 A750FXXU5CVI1 release-keys
    Signed-off-by: khushtaur3123 <khushtaur3123@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## a7y18ltejt-user 10 QP1A.190711.020 A750FXXU5CVI1 release-keys
+- Manufacturer: samsung
+- Platform: universal7885
+- Codename: a7y18lte
+- Brand: samsung
+- Flavor: a7y18ltejt-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: A750FXXU5CVI1
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: samsung/a7y18ltejt/a7y18lte:10/QP1A.190711.020/A750FXXU5CVI1:user/release-keys
+- OTA version: 
+- Branch: a7y18ltejt-user-10-QP1A.190711.020-A750FXXU5CVI1-release-keys
+- Repo: samsung_a7y18lte_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/priv-app/SamsungAvatarAuthoring_Lite/SamsungAvatarAuthoring_Lite.apk.* 2>/dev/null >> system/system/priv-app/SamsungAvatarAuthoring_Lite/SamsungAvatarAuthoring_Lite.apk
+rm -f system/system/priv-app/SamsungAvatarAuthoring_Lite/SamsungAvatarAuthoring_Lite.apk.* 2>/dev/null
+cat system/system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null >> system/system/priv-app/StickerFaceAR/StickerFaceAR.apk
+rm -f system/system/priv-app/StickerFaceAR/StickerFaceAR.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/BixbyService/BixbyService.apk.* 2>/dev/null >> system/system/priv-app/BixbyService/BixbyService.apk
+rm -f system/system/priv-app/BixbyService/BixbyService.apk.* 2>/dev/null
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
